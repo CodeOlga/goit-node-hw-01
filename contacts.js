064733adf70da77f66b76a705881e486597a7c48
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-09-09 18:29:46 +0300

    update js

--- contacts.js
+++ contacts.js
@@ -4,12 +4,10 @@ const contactsPath = path.join("db", "contacts.json");
 const { nanoid } = require("nanoid");
 
 async function listContacts() {
-  // ...твой код. Возвращает массив контактов.
   try {
     const jsonContactsRead = await fs.readFile(contactsPath);
     const contactsReadResult = JSON.parse(jsonContactsRead);
-    // console.log(contactsReadResult);
-    // console.table(contactsReadResult);
+
     return contactsReadResult;
   } catch (err) {
     console.log(err);
@@ -17,15 +15,12 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
   try {
-    // const jsonContactsRead = await fs.readFile(contactsPath);
-    // const contactsReadResult = JSON.parse(jsonContactsRead);
     const allContactsList = await listContacts();
     const foundContact = allContactsList.find(
       (contact) => contact.id === contactId
     );
-    // console.log(foundContact);
+
     return foundContact || null;
   } catch (err) {
     console.log(err);
@@ -33,34 +28,39 @@ async function getContactById(contactId) {
 }
 
 async function removeContact(contactId) {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-  const allContactsList = await listContacts();
-  const index = allContactsList.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (index === -1) {
-    return null;
+  try {
+    const allContactsList = await listContacts();
+    const index = allContactsList.findIndex(
+      (contact) => contact.id === contactId
+    );
+
+    if (index === -1) {
+      return null;
+    }
+
+    const [result] = allContactsList.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
+
+    return result;
+  } catch (err) {
+    console.log(err);
   }
-  const [result] = allContactsList.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
-  return result;
 }
 
 async function addContact({ name, email, phone }) {
-  // ...твой код. Возвращает объект добавленного контакта.
   try {
     const allContactsList = await listContacts();
+
     const newContact = {
       id: nanoid(),
-      // ...data,
       name,
       email,
       phone,
     };
+
     allContactsList.push(newContact);
-    // await fs.writeFile(contactsPath, JSON.stringify(allContactsList));
-    //щоб json був не в одну строку, число 2 тут - це виступи, таким чином запише нормального вигляду
     await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
+
     return newContact;
   } catch (err) {
     console.log(err);
--- index.js
+++ index.js
@@ -5,17 +5,9 @@ const {
   addContact,
 } = require("./contacts");
 
-// listContacts();
-// // getContactById("05olL7gyvQdWRwgKfg5J6"); //нереальный
-// getContactById("05olLMgyVQdWRwgKfg5J6"); //реальный
-// removeContact();
-// addContact();
-
-// const readline = require("readline");
-// require("colors");
-
 const { Command } = require("commander");
 const program = new Command();
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -27,32 +19,27 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторить
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
       const allContacts = await listContacts();
-      //return or break
       console.log(allContacts);
 
+      //return or break
       break;
 
     case "get":
-      // ... id
       const foundContactById = await getContactById(id);
       console.log(foundContactById);
 
       break;
 
     case "add":
-      // ... name email phone
       const newContact = await addContact({ name, email, phone });
       console.log(newContact);
       break;
 
     case "remove":
-      // ... id
       const removedContact = await removeContact(id);
       console.log(removedContact);
       break;
@@ -63,13 +50,3 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "e6ywwRe4jcqxXfCZOj_1e" });
-// invokeAction({
-//   action: "add",
-//   name: "Olga K",
-//   email: "olga@gmail.com",
-//   phone: "(777) 206-2688",
-// });
-// invokeAction({ action: "remove", id: "mfnIvp7y7bDI_-XEd4xoh" });
